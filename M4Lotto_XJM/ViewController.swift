51dfa966fcde1e49d40e2d606585e58066e9db1c
XJM <xander_moore1@mymail.eku.edu>
2026-02-10 11:41:46 -0500

    Initial Commit

---
+++ M4Lotto_XJM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M4Lotto_XJM
+//
+//  Created by Moore, Xander J. on 2/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
